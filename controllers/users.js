22ff983931ea35080ee447c266ce16956297740d
Balbaal <iqbal.mi20@gmail.com>
2020-09-28 01:19:47 +0800

    add: handle login user controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,9 @@
 const bcrypt = require("bcryptjs");
 const Validator = require("fastest-validator");
+const { findOne } = require("../models/User");
 const v = new Validator();
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET, JWT_ACCESS_TOKEN_EXPIRED } = process.env;
 
 // Model
 const userModel = require("../models/User");
@@ -10,10 +13,10 @@ module.exports = {
     const { name, email, password, role } = req.body;
 
     const schemaValidation = {
-      name: "string:min:3",
-      email: "string|unique",
-      password: "string|min:6",
-      role: "string",
+      name: "required|string:min:3",
+      email: "required|string|unique",
+      password: "required|string|min:6",
+      role: "required|string",
     };
 
     const resValidation = v.validate(req.body, schemaValidation);
@@ -56,7 +59,45 @@ module.exports = {
         .json({ status: "error", message: "something wrong with the server" });
     }
   },
-  login: (req, res) => {
-    res.send("login user");
+  login: async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const resUser = await findOne({ email });
+      if (!resUser) {
+        return res
+          .status(403)
+          .json({ status: "error", message: "email/password is wrong" });
+      }
+
+      const passwordIsMatch = await bcrypt.compare(password, resUser.password);
+      if (!passwordIsMatch) {
+        return res
+          .status(403)
+          .json({ status: "error", message: "email/password is wrong" });
+      }
+
+      const token = jwt.sign(
+        {
+          email: resUser.email,
+          id: resUser.id,
+          name: resUser.name,
+          role: resUser.role,
+        },
+        JWT_SECRET,
+        { expiresIn: JWT_ACCESS_TOKEN_EXPIRED }
+      );
+
+      res.status(200).json({
+        status: "success",
+        message: `login success`,
+        data: {
+          token,
+        },
+      });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ status: "error", message: "something wrong with the server" });
+    }
   },
 };
