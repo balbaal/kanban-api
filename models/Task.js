c8ee00b3e80141dc362c8b593a448906096c22f3
Balbaal <iqbal.mi20@gmail.com>
2020-09-28 14:30:43 +0800

    add: task model

---
+++ models/Task.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const taskSchema = {
+  taskTitle: {
+    type: String,
+    required: true,
+  },
+  taskDescription: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "new",
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+  projectId: {
+    type: ObjectId,
+    ref: "Project",
+  },
+};
+
+module.exports = mongoose.model("Task", taskSchema);
