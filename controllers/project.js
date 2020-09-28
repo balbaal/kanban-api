1b8a3019a54e4331b784dc1f5d5307493f021295
Balbaal <iqbal.mi20@gmail.com>
2020-09-28 08:53:53 +0800

    add: create project controller

--- controllers/project.js
+++ controllers/project.js
@@ -5,7 +5,36 @@ const v = new Validator();
 const projectModel = require("../models/Project");
 
 module.exports = {
-  createProject: (req, res) => {
-    res.json(req.body);
+  createProject: async (req, res) => {
+    const { projectName, userId } = req.body;
+
+    const schemaValidation = {
+      projectName: "String",
+      userId: "String",
+    };
+
+    const resValidation = v.validate(req.body, schemaValidation);
+    if (resValidation.length > 0) {
+      return res.status(400).json({
+        status: "error",
+        message: resValidation,
+      });
+    }
+
+    try {
+      const resProject = await projectModel.create({ projectName });
+      resProject.userId.push({ _id: userId });
+      await resProject.save();
+
+      res.status(201).json({
+        status: "success",
+        message: `project ${projectModel} is created`,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        message: `something wrong when create the project ${projectName}`,
+      });
+    }
   },
 };
